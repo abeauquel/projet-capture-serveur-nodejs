e654d0de5138c1059da3d98b4159558dc35e9251
abeauquel <beauquel.alexandre@gmail.com>
2018-11-14 14:55:06 -0500

    Feat  : Ajout d'une authentification par mot de passe

--- api/route/routesHumidite.js
+++ api/route/routesHumidite.js
@@ -2,25 +2,41 @@
 var controleurHumidite = require('../controleur/controleurHumidite');
 
 module.exports = function(app) {
+var motDePasse="paul";
 
     // humidite Routes
-
     app.get('/humidites' , (req, res) => {
+        if(req.headers.authentification !== motDePasse){
+            console.log("Routage humidite : accees refuse")
+            return res.status(401).send("Acces refuse");
+        }
         console.log('Routage humidite : get toutes les humidites');
        return controleurHumidite.listerHumites(req, res);
     });
 
     app.post('/humidite' , (req, res) => {
+        if(req.headers.authentification !== motDePasse){
+            console.log("Routage humidite : accees refuse")
+            return res.status(401).send("Acces refuse");
+        }
         console.log('Routage humidite : insertion d une humidite');
         return controleurHumidite.insererHumidite(req, res);
     });
 
     app.put('/humidite/:id' , (req, res) => {
+        if(req.headers.authentification !== motDePasse){
+            console.log("Routage humidite : accees refuse")
+            return res.status(401).send("Acces refuse");
+        }
         console.log('Routage humidite : modification d une humidite');
         return controleurHumidite.modificationHumidite(req, res);
     });
 
     app.delete('/humidite/:id' , (req, res) => {
+        if(req.headers.authentification !== motDePasse){
+            console.log("Routage humidite : accees refuse")
+            return res.status(401).send("Acces refuse");
+        }
         console.log('Routage humidite : suppression d une humidite');
         return controleurHumidite.suppressionHumidite(req, res);
     });
--- serveur.js
+++ serveur.js
@@ -5,13 +5,19 @@ import express from 'express';
 import "@babel/polyfill";
 
 
-dotenv.config();
+//dotenv.config();
 // const Reflection = process.env.TYPE === 'db' ? ReflectionWithDB : ReflectionWithJsObject;
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(express.json());
 
 app.get('/', (req, res) => {
+
     return res.status(200).send('Vous êtes bien connecté au serveur');
 });
 
